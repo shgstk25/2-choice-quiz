111bc688fa8064f4258c505be4769ac8847b3f9e
shogo <shgstk.0725@gmail.com>
2022-05-03 12:44:08 +0000

    router, storeを設定

---
+++ src/features/quiz.ts
@@ -0,0 +1,40 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Quiz } from "../data/quiz";
+
+export type QuizState = {
+    quizList: Quiz[]
+};
+
+const initialState: QuizState = {
+    quizList: [
+        {
+            id: 0,
+            text: "テストデータです。",
+            ans: true
+        }
+    ]
+};
+
+export const quizSlice = createSlice({
+    name: "quiz",
+    initialState,
+    reducers: {
+        added: (state, action: PayloadAction<Quiz>) => ({
+            ...state,
+            quizList: state.quizList.concat([action.payload])
+        }),
+        edited: (state, action: PayloadAction<Quiz>) => ({
+            ...state,
+            quizList: state.quizList.map((v) => {
+                if (v.id === action.payload.id) {
+                    return action.payload
+                }
+                return v
+            })
+        }),
+        removed: (state, action: PayloadAction<Quiz>) => ({
+            ...state,
+            quizList: state.quizList.filter((v) => v.id !== action.payload.id)
+        })
+    }
+})
